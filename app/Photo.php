252db0f4156a8aa9186e18899b9ed3c6efb468e7
konstantinos <kpetronikolos@gmail.com>
2018-03-30 16:40:14 +0300

    Uploading and displaying photos.

---
+++ app/Photo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    //
+	protected $uploads = '/images/';
+
+    protected $fillable = ['file'];
+
+	protected function getFileAttribute($photo) {
+		return $this->uploads . $photo;
+	}    
+}
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,6 +8,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -22,10 +23,13 @@
       	@foreach ($users as $user)
       		{{-- expr --}}
       		<tr>
-      			<td>{{$user->id}}</td>
+      			<td>{{$user->id}}</td>            
+            {{-- <td>{{$user->photo ? $user->photo->file : 'no user photo'}}</td> --}}
+            {{-- <td><img height="50" src="/images/{{$user->photo ? $user->photo->file : 'no user photo'}}" alt=""></td> --}}
+            <td><img height="50" src="{{$user->photo ? $user->photo->file : 'no user photo'}}" alt=""></td>
       			<td>{{$user->name}}</td>
       			<td>{{$user->email}}</td>
-      			<td>{{$user->role->name}}</td>
+      			<td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
       			<td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
       			<td>{{$user->created_at->diffForHumans()}}</td>
       			<td>{{$user->updated_at->diffForHumans()}}</td>
