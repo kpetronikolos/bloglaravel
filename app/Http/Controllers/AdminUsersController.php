b6983d66268675e02afb269edc53c0907a0eb6ef
konstantinos <kpetronikolos@gmail.com>
2018-03-31 04:30:11 +0300

    Setting up the form for edit users.

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -87,6 +87,11 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name', 'id')->all();
+        
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -27,7 +27,8 @@
             {{-- <td>{{$user->photo ? $user->photo->file : 'no user photo'}}</td> --}}
             {{-- <td><img height="50" src="/images/{{$user->photo ? $user->photo->file : 'no user photo'}}" alt=""></td> --}}
             <td><img height="50" src="{{$user->photo ? $user->photo->file : 'no user photo'}}" alt=""></td>
-      			<td>{{$user->name}}</td>
+      			{{-- <td>{{$user->name}}</td> --}}
+            <td><a href="{{ route('admin.users.edit', $user->id) }}">{{$user->name}}</a></td>
       			<td>{{$user->email}}</td>
       			<td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
       			<td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
