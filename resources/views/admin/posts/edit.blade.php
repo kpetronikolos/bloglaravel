c3164811abe2ed7fa04db510d6f827fe8c039130
konstantinos <kpetronikolos@gmail.com>
2018-04-11 15:14:32 +0300

    Edit and update post.

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,4 +3,36 @@
 @section('content')
 	<h1>Edit Post</h1>
 
+	{!! Form::model($post, ['method'=>'PATCH', 'action'=>['AdminPostsController@update', $post->id], 'files'=>'true']) !!}
+
+	<div class="form-group">
+		{!! Form::label('title', 'Title:') !!}
+		{!! Form::text('title', null, ['class'=>'form-control']) !!}		
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('category_id', 'Category:') !!}
+		{!! Form::select('category_id', [''=>'Choose categories'] + $categories, null, ['class'=>'form-control']) !!}		
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('photo_id', 'Photo:') !!}
+		{!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+		
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('body', 'Description:') !!}
+		{!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+		
+	</div>
+
+	<div class="form-group">
+		{!! Form::submit('Update Post', ['class'=>'btn btn-primary']) !!}
+	</div>
+
+	@include('includes.form_error')	
+
+	{!! Form::close() !!}
+
 @stop
