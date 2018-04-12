07e24af10d87d726d0d763be63f10912413960cd
konstantinos <kpetronikolos@gmail.com>
2018-04-12 18:24:37 +0300

    Delete images.

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -19,6 +19,15 @@
 					<td>{{$photo->id}}</td>
 					<td><img height="50" src="{{$photo->file}}" alt=""></td>
 					<td>{{$photo->created_at ? $photo->created_at : 'no date'}}</td>
+					<td>
+						{!! Form::open(['method'=>'DELETE', 'action'=>['AdminMediasController@destroy', $photo->id]]) !!}
+
+							<div class="form-group">
+								{!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+							</div>
+
+						{!! Form::close() !!}
+					</td>
 				</tr>
 			@endforeach
 		</tbody>
