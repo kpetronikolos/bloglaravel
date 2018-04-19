9c29128e810a67b434bbe8fceb506ed594e44f37
konstantinos <kpetronikolos@gmail.com>
2018-04-20 00:48:03 +0300

    View post and the comments of a post.

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -71,6 +71,11 @@ class PostCommentsController extends Controller
     public function show($id)
     {
         //
+        $post = Post::findOrFail($id);
+
+        $comments = $post->comments;
+
+        return view('admin.comments.show', compact('comments'));
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -18,6 +18,8 @@
 	        	<th>Category</th>	        	
 	        	<th>Title</th>
 	        	<th>Body</th>
+	        	<th>Post Link</th>
+	        	<th>Comments</th>
 	        	<th>Created</th>
 	        	<th>Updated</th>
 	      	</tr>
@@ -34,6 +36,8 @@
 	    				<td>{{$post->category ? $post->category->name : 'No category'}}</td>	    				
 	    				<td>{{$post->title}}</td>
 	    				<td>{{str_limit($post->body,7)}}</td>
+	    				<td><a href="{{ route('home.post', $post->id) }}">View Post</a></td>
+	    				<td><a href="{{ route('admin.comments.show', $post->id) }}">View Comments</a></td>
 	    				<td>{{$post->created_at->diffForHumans()}}</td>
 	    				<td>{{$post->updated_at->diffForHumans()}}</td>
 	    			</tr>
